68742913e1c7f65512c66e29c5aa1c7a7f4093da
DESKTOP-9MUFES4\K-B <k-b0514@hotmail.com>
2021-02-05 16:45:26 -0800

    02 - Basic Input Binding

--- ProjectDoge/Assets/Movement/Movement.cs
+++ ProjectDoge/Assets/Movement/Movement.cs
@@ -13,6 +13,43 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInput();
     }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Pressed SPACE - Thrusting");
+        }    
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("Pressed UP");
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("Pressed DOWN");
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Pressed RIGHT");
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Pressed LEFT");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Pressed D - Rotate Right");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Pressed A - Rotate Left");
+        }
+       
+    }
+
 }
